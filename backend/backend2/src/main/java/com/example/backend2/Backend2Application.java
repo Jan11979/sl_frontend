5917082855d8e4a441e2b9fe9f840469a07a1f95
Jan <Jan.Peltzer@iCloud.com>
2022-01-13 22:06:25 +0100

    asd

---
+++ backend/backend2/src/main/java/com/example/backend2/Backend2Application.java
@@ -0,0 +1,24 @@
+package com.example.backend2;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+@SpringBootApplication
+public class Backend2Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Backend2Application.class, args);
+    }
+
+
+//    @Override
+//    public void run(String... args) throws Exception {
+//    }
+
+}
